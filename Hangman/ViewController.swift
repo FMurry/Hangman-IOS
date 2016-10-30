4d44053235519fefb34d2a93928788997415fdb6
FMurry <frederic.wesley.murry@gmail.com>
2016-10-30 15:53:25 -0700

    Added Button functionality Changed health to hearts hangman correctly working

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -7,23 +7,24 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
     let dictionary : [Int: String] = [0: "Hello", 1: "World", 2: "Developer", 3: "Software", 4: "Apple", 5: "Mobile"]
     let TAG : String = "ViewController:"
-    
+    let alphabet : [String] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     var lives : Int = 10
+    var word : String!
     
     @IBOutlet weak var livesLabel : UILabel!
     @IBOutlet weak var hintLabel : UILabel!
-    @IBOutlet weak var userInput : UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let randomNumber : Int = Int(arc4random_uniform(5))
-        var word : String! = dictionary[randomNumber]
+        word = dictionary[randomNumber]
         print(TAG + " Word is:" + word)
         var i = word.characters.count
         var newLabel : String = ""
@@ -49,7 +50,7 @@ class ViewController: UIViewController {
         var livesToString : String = "Lives: "
         var i = lives
         while i > 0 {
-            livesToString = livesToString + "*"
+            livesToString = livesToString + "❤︎"
             i-=1
         }
         let length : Int = word.characters.count
@@ -58,6 +59,54 @@ class ViewController: UIViewController {
         livesLabel.text = livesToString
         print(TAG, "Done initializing game.....")
         
+    }
+    
+    @IBAction func game(sender : AnyObject) {
+        let button : UIButton = sender as! UIButton
+        let c = alphabet[button.tag]
+        print(TAG+c)
+        var i = 0
+        let characters = Array(word.characters)
+        print(TAG+"Characters:",characters)
+        var found : Bool = false
+        
+        for character in characters {
+            print(TAG+"char:",character)
+            if c == String(character).lowercased() {
+               found = true
+               print(TAG+"Match")
+                if i == 0{
+                    var label = [Character](hintLabel.text!.characters)
+                    label[0] = Character(c)
+                    let newLabel = String(label)
+                    hintLabel.text = newLabel
+                    
+                }
+                else{
+                    var label = [Character](hintLabel.text!.characters)
+                    label[i*2] = Character(c)
+                    let newLabel = String(label)
+                    hintLabel.text = newLabel
+                }
+            }
+            i+=1
+        }
+        if !found {
+            lives-=1
+            setHearts()
+            
+        }
+    }
+    
+    func setHearts() {
+        var livesToString : String = "Lives: "
+        var i = lives
+        while i > 0 {
+            livesToString = livesToString + "❤︎"
+            i-=1
+        }
+        print(TAG + " Lives are: " + livesToString)
+        livesLabel.text = livesToString
     }
 
 }
--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Created by Frederic Murry
 This is the Hangman project from CS175
 
 Play the game by entering a character
-Tested on Iphone
+Tested on Iphone 6, 6 plus, 7, 7 plus
 Completion Time: 1.5 hours
